2743e240797f7cfd4c5fbee2f5d86e53f7d2eb1d
Rachel Sakles <rachel.sakles@yahoo.com>
2020-09-08 14:36:40 -0700

    port changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require("uuid");
 const path = require("path");  
 
 const app = express(); 
-const PORT = 8080;  
+const PORT = process.env.PORT || 8080;   
 
 // let notes = [{
 
@@ -16,7 +16,7 @@ const PORT = 8080;
 //     title: "Hello", 
 //     text: "World!"
 // }]   
-// let notes = [];
+let notes = [];
 
 
 
